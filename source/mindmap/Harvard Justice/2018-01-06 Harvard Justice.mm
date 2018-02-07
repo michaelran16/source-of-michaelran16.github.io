e89ca6643eb076ae8217907dabe5fb76d686d598
Michael Ran <michaelran16@gmail.com>
2018-02-07 15:23:23 +0800

    change names of mind map files

---
+++ source/mindmap/Harvard Justice/2018-01-06 Harvard Justice.mm
@@ -0,0 +1,103 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1517888698548" ID="ID_603410992" MODIFIED="1517888890621" TEXT="Harvard Justice, Michael Sandel">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1517888764319" ID="ID_553257372" MODIFIED="1517888890598" POSITION="right" TEXT="Lecture 1: Doing the Right Thing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888781356" ID="ID_42784491" MODIFIED="1517888890600" POSITION="right" TEXT="Lecture 2: The Lifeboat Case">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888788718" ID="ID_729160823" MODIFIED="1517888890603" POSITION="right" TEXT="Lecture 3: Utilitarianism: Jeremy Bentham">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888793540" ID="ID_387089494" MODIFIED="1517888890604" POSITION="right" TEXT="Lecture 4: Utilitarianism: J.S. Mill">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888799809" ID="ID_1970495443" MODIFIED="1517888890605" POSITION="right" TEXT="Lecture 5: Libertarianism: Free Market Philosophy">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888805036" ID="ID_792037452" MODIFIED="1517888890606" POSITION="right" TEXT="Lecture 6: Libertarianism: Do We Own Ourselves">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888809556" ID="ID_1118568876" MODIFIED="1517888890607" POSITION="right" TEXT="Lecture 7: John Locke: Property Rights">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888813963" ID="ID_1259387879" MODIFIED="1517888890608" POSITION="right" TEXT="Lecture 8: John Locke: Individual Rights and Majority Rule">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888818067" ID="ID_1525925688" MODIFIED="1517888890609" POSITION="right" TEXT="Lecture 9: Markets and Morals: Military Service">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888823527" ID="ID_1418423752" MODIFIED="1517888890610" POSITION="right" TEXT="Lecture 10: Markets and Morals: Surrogate Motherhood">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888828066" ID="ID_955747310" MODIFIED="1517888890611" POSITION="right" TEXT="Lecture 11: Immanuel Kant: What is Freedom">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888832334" ID="ID_770645314" MODIFIED="1517888890612" POSITION="right" TEXT="Lecture 12: Immanuel Kant: The Supreme Principle of Morality">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888836945" ID="ID_1903467738" MODIFIED="1517888890614" POSITION="right" TEXT="Lecture 13: Immanuel Kant: A Lesson in Lying">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888841094" ID="ID_723447868" MODIFIED="1517888890614" POSITION="right" TEXT="Lecture 14: The Morality of Consent">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888845078" ID="ID_1881429881" MODIFIED="1517888890615" POSITION="right" TEXT="Lecture 15: John Rawls: The Case for Equality">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888848790" ID="ID_1470100418" MODIFIED="1517888890617" POSITION="right" TEXT="Lecture 16: Distributive Justice: Who Deserves What">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888854238" ID="ID_1839927052" MODIFIED="1517888890618" POSITION="right" TEXT="Lecture 17: Arguing Affirmative Action  ">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888864808" ID="ID_819935833" MODIFIED="1517888890619" POSITION="right" TEXT="Lecture 18: Aristotle: Justice and Virtue">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888869240" ID="ID_1435455458" MODIFIED="1517888890620" POSITION="right" TEXT="Lecture 19: Aristotle: The Good Citizen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888874109" ID="ID_29052345" MODIFIED="1517888890621" POSITION="right" TEXT="Lecture 20: Aristotle: Freedom vs. Fit">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888899558" ID="ID_1506716409" MODIFIED="1517888900450" POSITION="right" TEXT="Lecture 21: Justice, Community, and Membership">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888910149" ID="ID_1028196608" MODIFIED="1517888910766" POSITION="right" TEXT="Lecture 22: Dilemmas of Loyalty">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888914317" ID="ID_1904725927" MODIFIED="1517888914846" POSITION="right" TEXT="Lecture 23: Debating Same Sex-Marriage">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888918387" ID="ID_1971856990" MODIFIED="1517888918920" POSITION="right" TEXT="Lecture 24: Conclusion: Justice and the Good Life">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
