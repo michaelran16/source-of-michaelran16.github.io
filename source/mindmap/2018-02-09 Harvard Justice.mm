1cf90e5336f53db53dc8624b6593ca215a0575e7
Michael Ran <michaelran16@gmail.com>
2018-02-09 22:36:47 +0800

    edit justice mind map

---
+++ source/mindmap/2018-02-09 Harvard Justice.mm
@@ -0,0 +1,289 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1517888698548" ID="ID_603410992" MODIFIED="1518183467851" TEXT="Harvard Justice, Michael Sandel">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1517888781356" ID="ID_42784491" MODIFIED="1517888890600" POSITION="right" TEXT="Lecture 2: The Lifeboat Case">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1518184063948" ID="ID_1251566257" MODIFIED="1518184067120" TEXT="Case">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518184080339" ID="ID_92733199" MODIFIED="1518184099670" TEXT="1884 South Atlantic, 4 ppl">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518184117822" ID="ID_1480857207" MODIFIED="1518184149873" TEXT="Richard Parker, 17 appears to be dying after drunk seawater">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518184153260" ID="ID_1707431852" MODIFIED="1518184188994" TEXT="Day 19, captain suggested lottery but success">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518184198508" ID="ID_490683564" MODIFIED="1518184219463" TEXT="Next day, captain prayed and killed the boy">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518184229587" ID="ID_1102630403" MODIFIED="1518184289367" TEXT="Day 24, &quot;as we were having our breakfast,&quot; 3 man saved">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518184323428" ID="ID_1301008077" MODIFIED="1518184354439" TEXT="They claim &quot;out of necessity&quot;, ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1518185731442" ID="ID_1336216998" MODIFIED="1518185734424" TEXT="argument">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518185734952" ID="ID_899580834" MODIFIED="1518185979230" TEXT="objection 1, benefit vs loss">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518186164399" ID="ID_1382986745" MODIFIED="1518186184576" TEXT="if the killing is right before being saved?"/>
+<node COLOR="#111111" CREATED="1518186259405" ID="ID_1530948238" MODIFIED="1518186284675" TEXT="weaken the norm against murder">
+<node COLOR="#111111" CREATED="1518186299151" ID="ID_391385908" MODIFIED="1518186305347" TEXT="harder to recruit cabin boys"/>
+</node>
+<node COLOR="#111111" CREATED="1518186830073" ID="ID_524330241" MODIFIED="1518186838038" TEXT="follow the procedure?">
+<node COLOR="#111111" CREATED="1518186838479" ID="ID_1834043793" MODIFIED="1518186841797" TEXT="need consent"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518185762361" ID="ID_1940208959" MODIFIED="1518186058371" TEXT="objection 2, human rights">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518186390757" ID="ID_135426758" MODIFIED="1518186429505" TEXT="wrong to exploit a man&apos;s vulnerability"/>
+</node>
+<node COLOR="#990000" CREATED="1518186481464" ID="ID_134616160" MODIFIED="1518186488911" TEXT="Is morality">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518186489366" ID="ID_1219316317" MODIFIED="1518186504353" TEXT="counting lives and weighing costs and benefits"/>
+<node COLOR="#111111" CREATED="1518186505513" ID="ID_504540132" MODIFIED="1518186528361" TEXT="or certain moral duties are fundamental above calculation">
+<node COLOR="#111111" CREATED="1518186540453" ID="ID_1624096708" MODIFIED="1518186546911" TEXT="if so, how to identify"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1518184046466" ID="ID_569397570" MODIFIED="1518184047878" TEXT="Greatest Happiness Principle/Utilitarianism">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518186696902" ID="ID_1390868835" MODIFIED="1518186708822" TEXT="Bentham &#x8fb9;&#x6c81;&#xff08;&#x82f1;&#x56fd;&#x54f2;&#x5b66;&#x5bb6;&#xff09;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518186637430" ID="ID_1247154904" MODIFIED="1518186695542" TEXT="maximize utility">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518186656048" ID="ID_346093711" MODIFIED="1518186668070" TEXT="balance = pleasure - pain"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1517888788718" ID="ID_729160823" MODIFIED="1517888890603" POSITION="right" TEXT="Lecture 3: Utilitarianism: Jeremy Bentham">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888793540" ID="ID_387089494" MODIFIED="1517888890604" POSITION="right" TEXT="Lecture 4: Utilitarianism: J.S. Mill">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888799809" ID="ID_1970495443" MODIFIED="1517888890605" POSITION="right" TEXT="Lecture 5: Libertarianism: Free Market Philosophy">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888805036" ID="ID_792037452" MODIFIED="1517888890606" POSITION="right" TEXT="Lecture 6: Libertarianism: Do We Own Ourselves">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888809556" ID="ID_1118568876" MODIFIED="1517888890607" POSITION="right" TEXT="Lecture 7: John Locke: Property Rights">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888813963" ID="ID_1259387879" MODIFIED="1517888890608" POSITION="right" TEXT="Lecture 8: John Locke: Individual Rights and Majority Rule">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888818067" ID="ID_1525925688" MODIFIED="1517888890609" POSITION="right" TEXT="Lecture 9: Markets and Morals: Military Service">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888823527" ID="ID_1418423752" MODIFIED="1517888890610" POSITION="right" TEXT="Lecture 10: Markets and Morals: Surrogate Motherhood">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888828066" ID="ID_955747310" MODIFIED="1517888890611" POSITION="right" TEXT="Lecture 11: Immanuel Kant: What is Freedom">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888832334" ID="ID_770645314" MODIFIED="1517888890612" POSITION="right" TEXT="Lecture 12: Immanuel Kant: The Supreme Principle of Morality">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888836945" ID="ID_1903467738" MODIFIED="1517888890614" POSITION="right" TEXT="Lecture 13: Immanuel Kant: A Lesson in Lying">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888841094" ID="ID_723447868" MODIFIED="1517888890614" POSITION="right" TEXT="Lecture 14: The Morality of Consent">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888845078" ID="ID_1881429881" MODIFIED="1517888890615" POSITION="right" TEXT="Lecture 15: John Rawls: The Case for Equality">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888848790" ID="ID_1470100418" MODIFIED="1517888890617" POSITION="right" TEXT="Lecture 16: Distributive Justice: Who Deserves What">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888854238" ID="ID_1839927052" MODIFIED="1517888890618" POSITION="right" TEXT="Lecture 17: Arguing Affirmative Action  ">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888864808" ID="ID_819935833" MODIFIED="1517888890619" POSITION="right" TEXT="Lecture 18: Aristotle: Justice and Virtue">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888869240" ID="ID_1435455458" MODIFIED="1517888890620" POSITION="right" TEXT="Lecture 19: Aristotle: The Good Citizen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888874109" ID="ID_29052345" MODIFIED="1517888890621" POSITION="right" TEXT="Lecture 20: Aristotle: Freedom vs. Fit">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888899558" ID="ID_1506716409" MODIFIED="1517888900450" POSITION="right" TEXT="Lecture 21: Justice, Community, and Membership">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888910149" ID="ID_1028196608" MODIFIED="1517888910766" POSITION="right" TEXT="Lecture 22: Dilemmas of Loyalty">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888914317" ID="ID_1904725927" MODIFIED="1517888914846" POSITION="right" TEXT="Lecture 23: Debating Same Sex-Marriage">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888918387" ID="ID_1971856990" MODIFIED="1517888918920" POSITION="right" TEXT="Lecture 24: Conclusion: Justice and the Good Life">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517888764319" ID="ID_553257372" MODIFIED="1517888890598" POSITION="left" TEXT="Lecture 1: Doing the Right Thing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1518180927848" ID="ID_843825363" MODIFIED="1518181132642" TEXT="2 trolley car cases">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518179987900" ID="ID_115087888" MODIFIED="1518180938436" TEXT="Case 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518180000626" ID="ID_1795677562" MODIFIED="1518180938438" TEXT="5 works on track, only 1 on side track">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1518180037834" ID="ID_759853908" MODIFIED="1518180938451" TEXT="You can turn the trolley car">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518181002472" ID="ID_1177728185" MODIFIED="1518181013451" TEXT="seems moral?"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518180113162" ID="ID_501071747" MODIFIED="1518180938456" TEXT="Case 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518180142434" ID="ID_684431965" MODIFIED="1518182802812" TEXT="You standby, can push the heavy man">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518181016223" ID="ID_920622116" MODIFIED="1518181020202" TEXT="seems less moral?"/>
+</node>
+<node COLOR="#111111" CREATED="1518180156263" ID="ID_1578544750" MODIFIED="1518180938458" TEXT="You considered jumping yourself, but">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518182374051" ID="ID_1815463485" MODIFIED="1518182819660" TEXT="it&apos;s same case">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518180948952" ID="ID_1525767506" MODIFIED="1518182379726" TEXT="deliberate take innocent life, to prevent greater loss">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518180343606" ID="ID_322034242" MODIFIED="1518182359831" TEXT="wrong doing">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518180360198" ID="ID_220800377" MODIFIED="1518182359832" TEXT="against his will">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518180390317" ID="ID_1233708236" MODIFIED="1518180488169" TEXT="sacrifice on train track is not part of JD"/>
+</node>
+<node COLOR="#111111" CREATED="1518181084206" ID="ID_182457166" MODIFIED="1518182359833" TEXT="should we?">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518180630746" ID="ID_333553028" MODIFIED="1518181163073" TEXT="save as many lives as possible">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1518180805304" ID="ID_609784951" MODIFIED="1518181171194" TEXT="wrong to kill innocent, even for good cause">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1518181199562" ID="ID_22615197" MODIFIED="1518181209490" TEXT="Case with uncertainty">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518181224272" ID="ID_261340332" MODIFIED="1518181516052" TEXT="2005 Afghanistan">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518181253991" ID="ID_1592321701" MODIFIED="1518181266051" TEXT="4 US Navy SEALs">
+<node COLOR="#111111" CREATED="1518181246368" ID="ID_1031006767" MODIFIED="1518181251245" TEXT="search Taliban"/>
+<node COLOR="#111111" CREATED="1518181275127" ID="ID_1200765577" MODIFIED="1518181317605" TEXT="2 goatherds, unarmed civilians"/>
+</node>
+<node COLOR="#111111" CREATED="1518181431633" ID="ID_1242194972" MODIFIED="1518181499060" TEXT="Let them go, then 16 US soldiers killed"/>
+</node>
+<node COLOR="#990000" CREATED="1518181523279" ID="ID_1829987802" MODIFIED="1518182355404" TEXT="Uncertainty">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518181533175" ID="ID_1718221647" MODIFIED="1518182355405" TEXT="goatherd not 100% innocent">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518181622416" ID="ID_1706531879" MODIFIED="1518181659938" TEXT="eg. workers are French resistance, heavy man is Nazi"/>
+<node COLOR="#111111" CREATED="1518181666838" ID="ID_1539983595" MODIFIED="1518181675738" TEXT="morally compelling"/>
+</node>
+<node COLOR="#111111" CREATED="1518181825945" ID="ID_579913888" MODIFIED="1518182355405" TEXT="if goatherd is innocent">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518181841163" ID="ID_1174680591" MODIFIED="1518181856457" TEXT="but tortured by Taliban and reveal secret"/>
+<node COLOR="#111111" CREATED="1518181860855" ID="ID_1429017752" MODIFIED="1518181878362" TEXT="killing them can be morally questionable"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1518181920359" ID="ID_505682194" MODIFIED="1518181921971" TEXT="Moral Dilemmas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518181924976" ID="ID_1948500452" MODIFIED="1518182894684" TEXT="democratic society">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518181947889" ID="ID_666204637" MODIFIED="1518181952657" TEXT="abortion rights"/>
+<node COLOR="#111111" CREATED="1518181953080" ID="ID_598770393" MODIFIED="1518181972553" TEXT="fairness taxing on rich"/>
+<node COLOR="#111111" CREATED="1518181976510" ID="ID_1935994087" MODIFIED="1518181987288" TEXT="affirmative action in college admission"/>
+<node COLOR="#111111" CREATED="1518181989534" ID="ID_62430915" MODIFIED="1518181996272" TEXT="torture terror suspects"/>
+<node COLOR="#111111" CREATED="1518183412186" ID="ID_1459717877" MODIFIED="1518183413962" TEXT="Other">
+<node COLOR="#111111" CREATED="1518183394206" ID="ID_1423435007" MODIFIED="1518183445197" TEXT="Bailouts, same sex marrige, military service, price gouging"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518182051925" ID="ID_1460873443" MODIFIED="1518182904406" TEXT="Is moral convictions fixed?">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518182108310" ID="ID_1430439377" MODIFIED="1518182125874" TEXT="if so, moral persuasion is inconceivable"/>
+<node COLOR="#111111" CREATED="1518182425349" ID="ID_1958066641" MODIFIED="1518182957941" TEXT="dialectic moral argument">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1518182463669" ID="ID_1694698314" MODIFIED="1518182476607" TEXT="judgement about situation"/>
+<node COLOR="#111111" CREATED="1518182477143" ID="ID_1755496682" MODIFIED="1518182501072" TEXT="and principles we affirm"/>
+</node>
+<node COLOR="#111111" CREATED="1518182538587" ID="ID_1989502969" MODIFIED="1518182543551" TEXT="Seek a fit">
+<node COLOR="#111111" CREATED="1518182545973" ID="ID_253163246" MODIFIED="1518182564552" TEXT="lead us to justice and moral truth"/>
+<node COLOR="#111111" CREATED="1518182578501" ID="ID_164561559" MODIFIED="1518182594809" TEXT="is the result just self-consistent skein of prejudice?"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1518182693105" ID="ID_858916196" MODIFIED="1518182708936" TEXT="Moral reflection is not solitary, but public endeavor">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518182989707" ID="ID_1337512183" MODIFIED="1518183105180" TEXT="you need talker (can by imagined)"/>
+<node COLOR="#111111" CREATED="1518182508420" ID="ID_1406152889" MODIFIED="1518182950162" TEXT="Socrates and Aristotle">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1518183122379" ID="ID_16348089" MODIFIED="1518183197312" TEXT="Plato&apos;s Republic">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1518183201269" ID="ID_529979092" MODIFIED="1518183215260" TEXT="ordinary citizens = prisons in cave">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518183222412" ID="ID_1568113325" MODIFIED="1518183227540" TEXT="only see reflections"/>
+</node>
+<node COLOR="#990000" CREATED="1518183234968" ID="ID_930439662" MODIFIED="1518183243742" TEXT="philosopher">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518183250732" ID="ID_30953197" MODIFIED="1518183263317" TEXT="only one fit to rule"/>
+</node>
+<node COLOR="#990000" CREATED="1518183273644" ID="ID_1471417895" MODIFIED="1518183308335" TEXT="To grasp meaning of justice and nature, must rise above prejudice and everyday lift">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1518183311492" ID="ID_1916548152" MODIFIED="1518183321764" TEXT="moral reflection is dialectical">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1518183326889" ID="ID_1245812083" MODIFIED="1518183343103" TEXT="need options and convictions"/>
+</node>
+</node>
+</node>
+</node>
+</map>
